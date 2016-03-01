9a419bb0a60438b598965846357ebed9fd5bc10a
unknown <BABIKBE1@PHCZPR-L50092.EU.novartis.net>
2016-03-01 16:17:55 +0100

    'foo'

---
+++ install.js
@@ -0,0 +1,107 @@
+"use strict"
+var async = require('async');
+var Seneca = require('seneca');
+
+var seneca = Seneca({timeout:10000});
+seneca.client({host:'localhost', port:10101});
+
+async.series([
+  done => {
+    seneca.act({
+      role: 'data',
+      cmd: 'configure',
+      config: {
+        database: 'MobilityInsightsDev'
+      }
+    }, function(error, response) {
+      if (error)
+        throw error;
+      done();
+    });
+  },
+  done => {
+    seneca.act({
+      role: 'data',
+      cmd: 'saveDashboard',
+      dashboard: {
+        name: 'default',
+        description: 'Default Dashboard',
+        background: 'white',
+        itemRenderTemplates: [
+          { name: 'notice', source: '<div><div render-target></div></div>' }
+        ],
+        articles: [
+          { name: 'Mobility Quarter Report - Q4 2015',
+            iconBackground: 'orange',
+            iconTemplate: 'group',
+            articles: [
+              {
+                name: 'Major accomplishments',
+                content: {
+                  type: 'link',
+                  data: 'http://192.37.61.19/_MobiReport_dashboard/Q42015/02.html'
+                }
+              },
+              {
+                name: 'Corporate devices overview',
+                content: {
+                  type: 'link',
+                  data: 'http://192.37.61.19/_MobiReport_dashboard/Q42015/03.html'
+                }
+              },
+              {
+                name: 'Distribution of corporate devices',
+                content: {
+                  type: 'link',
+                  data: 'http://192.37.61.19/_MobiReport_dashboard/Q42015/04.html'
+                }
+              },
+              {
+                name: 'BYOM',
+                content: {
+                  type: 'link',
+                  data: 'http://192.37.61.19/_MobiReport_dashboard/Q42015/05.html'
+                }
+              },
+              {
+                name: 'Mobile Apps Overview',
+                content: {
+                  type: 'link',
+                  data: 'http://192.37.61.19/_MobiReport_dashboard/Q42015/06.html'
+                }
+              },
+              {
+                name: 'Incident Management',
+                content: {
+                  type: 'link',
+                  data: 'http://192.37.61.19/_MobiReport_dashboard/Q42015/07.html'
+                }
+              },
+              {
+                name: 'Mobility Supporter Tool',
+                content: {
+                  type: 'link',
+                  data: 'http://192.37.61.19/_MobiReport_dashboard/Q42015/08.html'
+                }
+              },
+
+            ]
+          },
+        ],
+        permissions: {
+          readers: {
+            groups: [ { name: 'everyone' } ]
+          }
+        }
+      }
+    }, function(error, response) {
+      if (error)
+        throw error;
+      done();
+    });
+  }
+], error => {
+  if (error)
+    throw error;
+  seneca.close();
+});
---
+++ platform/rest/index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+// TODO get version from config
+var _pfx = '/api/v1';
+
+var express = require('express');
+var namespace = require('express-namespace');
+var seneca = require('seneca')({ timeout: 10000 });
+var util = require('util');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var async = require('async');
+var extend = require('extend');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+seneca.client({ host: 'localhost', port: 10101 });
+var data = {
+  act: (request, callback) => {
+    extend(request, { role:'data' });
+    seneca.act(request, callback);
+  }
+};
+
+app.namespace(_pfx, () => {
+  app.get('/ok', (req, res) => {
+    res.send({ status:'ok' });
+    // TODO include db status etc.
+  });
+  app.get('/dashboard', (req, res) => {
+    console.log('getDashboard');
+    data.act({
+      cmd:'getDashboard',
+      dashboard: {
+        name: 'default'
+        // TODO user and group information
+      }
+    }, (error, response) => {
+      console.log('response');
+      if (error)
+        throw error;
+      // TODO sanitize
+      res.send(response);
+    });
+  });
+
+
+});
+
+console.log('starting server');
+app.listen(3001);
